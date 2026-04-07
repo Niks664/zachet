4324ec4c1ec16fbc1984e090f57fcd09854d37a8
TereNce_G <sash.tockarew2018@yandex.ru>
2026-04-07 09:28:09 +0300

    input and validation

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def Timer(seconds):
-    for i in seconds:
-        print(seconds)
-        
+def get_input(prompt):
+    while True:
+        text = input(prompt)
+        if text.isdigit():
+            return int(text)
+        print("Ошибка! Введи число.")
+
+print("Настройка Таймера")
+hours = get_input("Введите часы: ")
+minutes = get_input("Введите минуты: ")
+seconds = get_input("Введите секунды: ")
+
+total_seconds = hours * 3600 + minutes * 60 + seconds
+
+print("\nТаймер запущен!")
